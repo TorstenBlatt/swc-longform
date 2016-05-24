c8218eeedc2232094e9ac1a6ab8da7f430490196
Torsten Blatt <t.blatt@keyseven.de>
2016-05-24 23:06:55 +0200

    Anpassungen mobile Menu

--- longform-adaption/js/plugins.js
+++ longform-adaption/js/plugins.js
@@ -33,7 +33,7 @@ var menuList = menu.find('ul:first');
 var listItems = menu.find('li').not('#responsive-tab');
 
 // Create responsive trigger
-menuList.prepend('<li id="responsive-tab"><a href="#">&nbsp;</a></li>');
+menuList.prepend('<li id="responsive-tab"><a href="#" style="color:#2d90ec;">Bluewin</a></li>');
 
 // Toggle menu visibility
 menu.on('click', '#responsive-tab', function(event){
